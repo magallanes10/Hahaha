b4046a9b7c44b9c8cc5345f017f99e9f198a9def
magallanes10 <125812726+magallanes10@users.noreply.github.com>
2023-12-20 14:18:26 +0800

    Update listen.js

--- includes/listen.js
+++ includes/listen.js
@@ -1,100 +1,24 @@
 module.exports = function({ api, models }) {
 
-  const Users = require("./controllers/users")({ models, api }),
-        Threads = require("./controllers/threads")({ models, api }),
-        Currencies = require("./controllers/currencies")({ models });
-  const logger = require("../utils/log.js");
-  const fs = require("fs");
-  const moment = require('moment-timezone');
-  const axios = require("axios");
-  var day = moment.tz("Asia/Manila").day();
+	const Users = require("./controllers/users")({ models, api }),
+				Threads = require("./controllers/threads")({ models, api }),
+				Currencies = require("./controllers/currencies")({ models });
+	const logger = require("../utils/log.js");
+	const fs = require("fs");
+	const moment = require('moment-timezone');
+	const axios = require("axios");
+
+	//////////////////////////////////////////////////////////////////////
+	//========= Push all variable from database to environment =========//
+	//////////////////////////////////////////////////////////////////////
 
 
-  const checkttDataPath = __dirname + '/../script/commands/checktt/';
-  setInterval(async() => {
-    const day_now = moment.tz("Asia/Manila").day();
-    if (day != day_now) {
-      day = day_now;
-      const checkttData = fs.readdirSync(checkttDataPath);
-      console.log('--> CHECKTT: New Day');
-      checkttData.forEach(async(checkttFile) => {
-        const checktt = JSON.parse(fs.readFileSync(checkttDataPath + checkttFile));
-        let storage = [], count = 1;
-        for (const item of checktt.day) {
-            const userName = await Users.getNameUser(item.id) || 'Facebook User';
-            const itemToPush = item;
-            itemToPush.name = userName;
-            storage.push(itemToPush);
-        };
-        storage.sort((a, b) => {
-            if (a.count > b.count) {
-                return -1;
-            }
-            else if (a.count < b.count) {
-                return 1;
-            } else {
-                return a.name.localeCompare(b.name);
-            }
-        });
-        let checkttBody = '===Top 10 Interactive Days===\n';
-        checkttBody += storage.slice(0, 10).map(item => {
-          return `${count++}. ${item.name} (${item.count})`;
-      }).join('\n');
-        api.sendMessage(checkttBody, checkttFile.replace('.json', ''), (err) => err ? console.log(err) : '');
-
-        checktt.day.forEach(e => {
-            e.count = 0;
-        });
-        checktt.time = day_now;
-
-        fs.writeFileSync(checkttDataPath + checkttFile, JSON.stringify(checktt, null, 4));
-      });
-      if (day_now == 1) {
-        console.log('--> CHECKTT: New Week');
-        checkttData.forEach(async(checkttFile) => {
-          const checktt = JSON.parse(fs.readFileSync(checkttDataPath + checkttFile));
-          let storage = [], count = 1;
-          for (const item of checktt.week) {
-              const userName = await Users.getNameUser(item.id) || 'Facebook User';
-              const itemToPush = item;
-              itemToPush.name = userName;
-              storage.push(itemToPush);
-          };
-          storage.sort((a, b) => {
-              if (a.count > b.count) {
-                  return -1;
-              }
-              else if (a.count < b.count) {
-                  return 1;
-              } else {
-                  return a.name.localeCompare(b.name);
-              }
-          });
-          let checkttBody = '===Top 10 Interactive Week===\n';
-          checkttBody += storage.slice(0, 10).map(item => {
-            return `${count++}. ${item.name} (${item.count})`;
-        }).join('\n');
-          api.sendMessage(checkttBody, checkttFile.replace('.json', ''), (err) => err ? console.log(err) : '');
-          checktt.week.forEach(e => {
-              e.count = 0;
-          });
-
-          fs.writeFileSync(checkttDataPath + checkttFile, JSON.stringify(checktt, null, 4));
-        })
-      }
-      global.client.sending_top = false;
-    }
-  }, 1000 * 10);
-
-
-  //////////////////////////////////////////////////////////////////////
-  //========= Push all variable from database to environment =========//
-  //////////////////////////////////////////////////////////////////////
-
+  
 (async function () {
+  api.markAsReadAll((err) => {})
 
     try {
-        logger(global.getText('listen', 'startLoadEnvironment'), '[ DATABASE ]');
+      logger(global.getText('listen', 'startLoadEnvironment'), '[ DATABASE ]');
         let threads = await Threads.getAll(),
             users = await Users.getAll(['userID', 'name', 'data']),
             currencies = await Currencies.getAll(['userID']);
@@ -104,8 +28,8 @@ module.exports = function({ api, models }) {
             global.data.threadData.set(idThread, data['data'] || {}), 
             global.data.threadInfo.set(idThread, data.threadInfo || {});
             if (data['data'] && data['data']['banned'] == !![]) 
-              global.data.threadBanned.set(idThread, 
-              {
+            	global.data.threadBanned.set(idThread, 
+            	{
                 'reason': data['data']['reason'] || '',
                 'dateAdded': data['data']['dateAdded'] || ''
             });
@@ -131,174 +55,161 @@ module.exports = function({ api, models }) {
         return logger.loader(global.getText('listen', 'failLoadEnvironment', error), 'error');
     }
 }());
-  logger(`${api.getCurrentUserID()} - [ ${global.config.PREFIX} ] • ${(!global.config.BOTNAME) ? "This bot was made by CatalizCS, SpermLord, Choru And John Arida" : global.config.BOTNAME}`, "[ BOT INFO ]");
+	logger(`${api.getCurrentUserID()} - [ ${global.config.PREFIX} ] • ${(!global.config.BOTNAME) ? "This bot was made by CatalizCS and SpermLord" : global.config.BOTNAME}`, "[ BOT INFO ]");
 
-  ///////////////////////////////////////////////
-  //========= Require all handle need =========//
-  //////////////////////////////////////////////
+  
+	
+	///////////////////////////////////////////////
+	//========= Require all handle need =========//
+	//////////////////////////////////////////////
 
-  const handleCommand = require("./handle/handleCommand")({ api, models, Users, Threads, Currencies });
-  const handleCommandEvent = require("./handle/handleCommandEvent")({ api, models, Users, Threads, Currencies });
-  const handleReply = require("./handle/handleReply")({ api, models, Users, Threads, Currencies });
-  const handleReaction = require("./handle/handleReaction")({ api, models, Users, Threads, Currencies });
-  const handleEvent = require("./handle/handleEvent")({ api, models, Users, Threads, Currencies });
-  const handleCreateDatabase = require("./handle/handleCreateDatabase")({  api, Threads, Users, Currencies, models });
+	const handleCommand = require("./handle/handleCommand")({ api, models, Users, Threads, Currencies });
+	const handleCommandEvent = require("./handle/handleCommandEvent")({ api, models, Users, Threads, Currencies });
+	const handleReply = require("./handle/handleReply")({ api, models, Users, Threads, Currencies });
+	const handleReaction = require("./handle/handleReaction")({ api, models, Users, Threads, Currencies });
+	const handleEvent = require("./handle/handleEvent")({ api, models, Users, Threads, Currencies });
+	const handleCreateDatabase = require("./handle/handleCreateDatabase")({  api, Threads, Users, Currencies, models });
 
-  logger.loader(`====== ${Date.now() - global.client.timeStart}ms ======`);
-
-
-  //DEFINE DATLICH PATH
-  const datlichPath = __dirname + '/../modules/commands/cache/datlich.json';
-
-  //FUNCTION HOẠT ĐỘNG NHƯ CÁI TÊN CỦA NÓ, CRE: DUNGUWU
-  const monthToMSObj = {
-    1: 31 * 24 * 60 * 60 * 1000,
-    2: 28 * 24 * 60 * 60 * 1000,
-    3: 31 * 24 * 60 * 60 * 1000,
-    4: 30 * 24 * 60 * 60 * 1000,
-    5: 31 * 24 * 60 * 60 * 1000,
-    6: 30 * 24 * 60 * 60 * 1000,
-    7: 31 * 24 * 60 * 60 * 1000,
-    8: 31 * 24 * 60 * 60 * 1000,
-    9: 30 * 24 * 60 * 60 * 1000,
-    10: 31 * 24 * 60 * 60 * 1000,
-    11: 30 * 24 * 60 * 60 * 1000,
-    12: 31 * 24 * 60 * 60 * 1000
-  };
-  const checkTime = (time) => new Promise((resolve) => {
-    time.forEach((e, i) => time[i] = parseInt(String(e).trim()));
-    const getDayFromMonth = (month) => (month == 0) ? 0 : (month == 2) ? (time[2] % 4 == 0) ? 29 : 28 : ([1, 3, 5, 7, 8, 10, 12].includes(month)) ? 31 : 30;
-    if (time[1] > 12 || time[1] < 1) resolve("Your month doesn't seem valid");
-    if (time[0] > getDayFromMonth(time[1]) || time[0] < 1) resolve("Your date seems invalid");
-    if (time[2] < 2022) resolve("In what era do you live in?");
-    if (time[3] > 23 || time[3] < 0) resolve("Your hours don't seem valid");
-    if (time[4] > 59 || time[3] < 0) resolve("Your minutes don't seem valid");
-    if (time[5] > 59 || time[3] < 0) resolve("Your seconds don't seem valid");
-    yr = time[2] - 1970;
-    yearToMS = (yr) * 365 * 24 * 60 * 60 * 1000;
-    yearToMS += ((yr - 2) / 4).toFixed(0) * 24 * 60 * 60 * 1000;
-    monthToMS = 0;
-    for (let i = 1; i < time[1]; i++) monthToMS += monthToMSObj[i];
-    if (time[2] % 4 == 0) monthToMS += 24 * 60 * 60 * 1000;
-    dayToMS = time[0] * 24 * 60 * 60 * 1000;
-    hourToMS = time[3] * 60 * 60 * 1000;
-    minuteToMS = time[4] * 60 * 1000;
-    secondToMS = time[5] * 1000;
-    oneDayToMS = 24 * 60 * 60 * 1000;
-    timeMs = yearToMS + monthToMS + dayToMS + hourToMS + minuteToMS + secondToMS - oneDayToMS;
-    resolve(timeMs);
-  });
+	logger.loader(`====== ${Date.now() - global.client.timeStart}ms ======`);
 
 
-  const tenMinutes = 10 * 60 * 1000;
+	//DEFINE DATLICH PATH
+	const datlichPath = __dirname + '/../modules/commands/cache/datlich.json';
 
-  logger.loader(`====== ${Date.now() - global.client.timeStart}ms ======`);
-  const checkAndExecuteEvent = async () => {
-
-    /*smol check*/
-    if (!fs.existsSync(datlichPath)) fs.writeFileSync(datlichPath, JSON.stringify({}, null, 4));
-    var data = JSON.parse(fs.readFileSync(datlichPath));
-
-    //GET CURRENT TIME
-    var timeVN = moment().tz('Asia/Manila').format('Manila:mm:ss');
-    timeVN = timeVN && timeVN[0] ? [...timeVN[0].split("/"), ...timeVN[1].split(":")] : [];
-
-    let temp = [];
-    let vnMS = await checkTime(timeVN);
-    const compareTime = e => new Promise(async (resolve) => {
-      let getTimeMS = await checkTime(e.split("_"));
-      if (getTimeMS < vnMS) {
-        if (vnMS - getTimeMS < tenMinutes) {
-          data[boxID][e]["TID"] = boxID;
-          temp.push(data[boxID][e]); delete data[boxID][e];
-        } else delete data[boxID][e];
-        fs.writeFileSync(datlichPath, JSON.stringify(data, null, 4));
-      };
-      resolve();
-    })
-
-    await new Promise(async (resolve) => {
-      for (boxID in data) {
-        for (e of Object.keys(data[boxID])) await compareTime(e);
-      }
-      resolve();
-    })
-    for (el of temp) {
-      try {
-        var all = (await Threads.getInfo(el["TID"])).participantIDs;
-          all.splice(all.indexOf(api.getCurrentUserID()), 1);
-        var body = el.REASON || "MỌI NGƯỜI ƠI", mentions = [], index = 0;
-
-          for (let i = 0; i < all.length; i++) {
-            if (i == body.length) body += " ‍ ";
-            mentions.push({
-              tag: body[i],
-              id: all[i],
-              fromIndex: i - 1
-            });
-          }
-      } catch (e) { return console.log(e); }
-      var out = {
-        body, mentions
-      }
-      if ("ATTACHMENT" in el) {
-        out.attachment = [];
-        for (a of el.ATTACHMENT) {
-          let getAttachment = (await axios.get(encodeURI(a.url), { responseType: "arraybuffer"})).data;
-          fs.writeFileSync(__dirname + `/../script/commands/cache/${a.fileName}`, Buffer.from(getAttachment, 'utf-8'));
-          out.attachment.push(fs.createReadStream(__dirname + `/../script/commands/cache/${a.fileName}`));
-        }
-      }
-      console.log(out);
-      if ("BOX" in el) await api.setTitle(el["BOX"], el["TID"]);
-      api.sendMessage(out, el["TID"], () => ("ATTACHMENT" in el) ? el.ATTACHMENT.forEach(a => fs.unlinkSync(__dirname + `/../script/commands/cache/${a.fileName}`)) : "");
-    }
-
-  }
-  setInterval(checkAndExecuteEvent, tenMinutes/10);
+	//FUNCTION HOẠT ĐỘNG NHƯ CÁI TÊN CỦA NÓ, CRE: DUNGUWU
+	const monthToMSObj = {
+		1: 31 * 24 * 60 * 60 * 1000,
+		2: 28 * 24 * 60 * 60 * 1000,
+		3: 31 * 24 * 60 * 60 * 1000,
+		4: 30 * 24 * 60 * 60 * 1000,
+		5: 31 * 24 * 60 * 60 * 1000,
+		6: 30 * 24 * 60 * 60 * 1000,
+		7: 31 * 24 * 60 * 60 * 1000,
+		8: 31 * 24 * 60 * 60 * 1000,
+		9: 30 * 24 * 60 * 60 * 1000,
+		10: 31 * 24 * 60 * 60 * 1000,
+		11: 30 * 24 * 60 * 60 * 1000,
+		12: 31 * 24 * 60 * 60 * 1000
+	};
+	const checkTime = (time) => new Promise((resolve) => {
+		time.forEach((e, i) => time[i] = parseInt(String(e).trim()));
+		const getDayFromMonth = (month) => (month == 0) ? 0 : (month == 2) ? (time[2] % 4 == 0) ? 29 : 28 : ([1, 3, 5, 7, 8, 10, 12].includes(month)) ? 31 : 30;
+		if (time[1] > 12 || time[1] < 1) resolve("Tháng của bạn có vẻ không hợp lệ");
+		if (time[0] > getDayFromMonth(time[1]) || time[0] < 1) resolve("Ngày của bạn có vẻ không hợp lệ");
+		if (time[2] < 2022) resolve("You live at the Kỷ nguyên nào thế giới?");
+		if (time[3] > 23 || time[3] < 0) resolve("Giờ của bạn có vẻ không hợp lệ");
+		if (time[4] > 59 || time[3] < 0) resolve("Phút của bạn có vẻ không hợp lệ");
+		if (time[5] > 59 || time[3] < 0) resolve("Giây của bạn có vẻ không hợp lệ");
+		yr = time[2] - 1970;
+		yearToMS = (yr) * 365 * 24 * 60 * 60 * 1000;
+		yearToMS += ((yr - 2) / 4).toFixed(0) * 24 * 60 * 60 * 1000;
+		monthToMS = 0;
+		for (let i = 1; i < time[1]; i++) monthToMS += monthToMSObj[i];
+		if (time[2] % 4 == 0) monthToMS += 24 * 60 * 60 * 1000;
+		dayToMS = time[0] * 24 * 60 * 60 * 1000;
+		hourToMS = time[3] * 60 * 60 * 1000;
+		minuteToMS = time[4] * 60 * 1000;
+		secondToMS = time[5] * 1000;
+		oneDayToMS = 24 * 60 * 60 * 1000;
+		timeMs = yearToMS + monthToMS + dayToMS + hourToMS + minuteToMS + secondToMS - oneDayToMS;
+		resolve(timeMs);
+	});
 
 
-  //////////////////////////////////////////////////
-  //========= Send event to handle need =========//
-  /////////////////////////////////////////////////
+	const tenMinutes = 10 * 60 * 1000;
 
-  return (event) => {
-    if (event.type == "change_thread_image") api.sendMessage(`» [ GROUP UPDATES ] ${event.snippet}`, event.threadID);
-    let data = JSON.parse(fs.readFileSync(__dirname + "/../script/commands/cache/approvedThreads.json"));
-    let adminBot = global.config.ADMINBOT
-if (Array.isArray(data) && !data.includes(event.threadID) && !adminBot.includes(event.senderID)) {
+	logger.loader(`====== ${Date.now() - global.client.timeStart}ms ======`);
+	const checkAndExecuteEvent = async () => {
 
-      //getPrefix
-      const threadSetting = global.data.threadData.get(parseInt(event.threadID)) || {};
-      const prefix = (threadSetting.hasOwnProperty("PREFIX")) ? threadSetting.PREFIX : global.config.PREFIX;
+		/*smol check*/
+		if (!fs.existsSync(datlichPath)) fs.writeFileSync(datlichPath, JSON.stringify({}, null, 4));
+		var data = JSON.parse(fs.readFileSync(datlichPath));
 
-      //check body
-      if (event.body && event.body == `${prefix}request`) {
-        adminBot.forEach(e => {
-          api.sendMessage(`» ID: ${event.threadID}\n» Requested For Approval! `, e);
-        })
-        return api.sendMessage(`Your Request Has Been Successfully Sent To The Admins, Please Wait For Them To Approve It First, Before Using Me, Thank You!`, event.threadID);
-      }
-      if (event.body && event.body.startsWith(prefix)) return api.sendMessage(`Your Box Has Not Been Approved Yet. To Submit A Request For Approval, Please Type On Your Thread: ${prefix}request`, event.threadID);
-    };
-    switch (event.type) {
-      case "message":
-      case "message_reply":
-      case "message_unsend":
-        handleCreateDatabase({ event });
-        handleCommand({ event });
-        handleReply({ event });
-        handleCommandEvent({ event });
+		//GET CURRENT TIME
+		var timeVN = moment().tz('Asia/Ho_Chi_Minh').format('DD/MM/YYYY_HH:mm:ss');
+		timeVN = timeVN.split("_");
+		timeVN = [...timeVN[0].split("/"), ...timeVN[1].split(":")];
 
-        break;
-      case "event":
-        handleEvent({ event });
-        break;
-      case "message_reaction":
-        handleReaction({ event });
-        break;
-      default:
-        break;
-    }
-  };
+		let temp = [];
+		let vnMS = await checkTime(timeVN);
+		const compareTime = e => new Promise(async (resolve) => {
+			let getTimeMS = await checkTime(e.split("_"));
+			if (getTimeMS < vnMS) {
+				if (vnMS - getTimeMS < tenMinutes) {
+					data[boxID][e]["TID"] = boxID;
+					temp.push(data[boxID][e]); delete data[boxID][e];
+				} else delete data[boxID][e];
+				fs.writeFileSync(datlichPath, JSON.stringify(data, null, 4));
+			};
+			resolve();
+		})
+
+		await new Promise(async (resolve) => {
+			for (boxID in data) {
+				for (e of Object.keys(data[boxID])) await compareTime(e);
+			}
+			resolve();
+		})
+		for (el of temp) {
+			try {
+				var all = (await Threads.getInfo(el["TID"])).participantIDs;
+			    all.splice(all.indexOf(api.getCurrentUserID()), 1);
+				var body = el.REASON || "MỌI NGƯỜI ƠI", mentions = [], index = 0;
+				
+			    for (let i = 0; i < all.length; i++) {
+				    if (i == body.length) body += " ‍ ";
+				    mentions.push({
+				  	  tag: body[i],
+				  	  id: all[i],
+				  	  fromIndex: i - 1
+				    });
+			    }
+			} catch (e) { return console.log(e); }
+			var out = {
+				body, mentions
+			}
+			if ("ATTACHMENT" in el) {
+				out.attachment = [];
+				for (a of el.ATTACHMENT) {
+					let getAttachment = (await axios.get(encodeURI(a.url), { responseType: "arraybuffer"})).data;
+					fs.writeFileSync(__dirname + `/../modules/commands/cache/${a.fileName}`, Buffer.from(getAttachment, 'utf-8'));
+					out.attachment.push(fs.createReadStream(__dirname + `/../modules/commands/cache/${a.fileName}`));
+				}
+			}
+			console.log(out);
+			if ("BOX" in el) await api.setTitle(el["BOX"], el["TID"]);
+			api.sendMessage(out, el["TID"], () => ("ATTACHMENT" in el) ? el.ATTACHMENT.forEach(a => fs.unlinkSync(__dirname + `/../modules/commands/cache/${a.fileName}`)) : "");
+		}
+
+	}
+	setInterval(checkAndExecuteEvent, tenMinutes/10);
+	
+
+	//////////////////////////////////////////////////
+	//========= Send event to handle need =========//
+	/////////////////////////////////////////////////
+	
+	return (event) => {
+		switch (event.type) {
+			case "message":
+			case "message_reply":
+			case "message_unsend":
+				handleCreateDatabase({ event });
+				handleCommand({ event });
+				handleReply({ event });
+				handleCommandEvent({ event });
+
+				break;
+			case "event":
+				handleEvent({ event });
+				break;
+			case "message_reaction":
+				handleReaction({ event });
+				break;
+			default:
+				break;
+		}
+	};
 };
+
+//THIZ BOT WAS MADE BY ME(CATALIZCS) AND MY BROTHER SPERMLORD - DO NOT STEAL MY CODE (つ ͡ ° ͜ʖ ͡° )つ ✄ ╰⋃╯

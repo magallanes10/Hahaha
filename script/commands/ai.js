e44e16f3f412d97f5ca0882c82d455e2b1b9d172
Jonell Magallanes <125812726+magallanes10@users.noreply.github.com>
2023-12-31 17:51:49 +0800

    Update ai.js

--- script/commands/ai.js
+++ script/commands/ai.js
@@ -1,49 +1,32 @@
-const axios = require("axios");
+const axios = require('axios');
 
 module.exports.config = {
-  name: "ai",
-  version: "1.0.0",
-  hasPermission: 0, 
-  credits: "JV Barcenas & cyril", // Converted and modified to mirai by cyril //dont change credits or I spank your ass
-  description: "ChatGPT-4",
-  usePrefix: false,
-  aliases: "chatgpt",
-  commandCategory: "ChatBots",
+  name: 'ai',
+  version: '1.0.0',
+  hasPermission: 0,
+  credits: 'Jonell Magallanes',
+  description: 'Ask a question to AI',
+  commandCategory: 'ai',
+  usages: ['ai <your question>'],
   cooldowns: 20,
+  usePrefix: false,
 };
 
-module.exports.run = async function ({ api, args, event }) {
-  try {
-    const prompt = event.body.trim();
-    const { threadID, messageID } = event;
-
-    if (!args[0]) {
-      api.sendMessage(
-        "Please provide a question to answer\n\nExample:\nai what is solar system?",
-        threadID, 
-        messageID 
-      );
-      return;
-    }
-
-    if (prompt) {
-      await api.sendMessage("🔍 | Searching and Typing Your Answer! Please Wait....", threadID);
-
-      const response = await axios.get(`https://chatgayfeyti.archashura.repl.co?gpt=${encodeURIComponent(prompt)}`);
-
-      if (response.status === 200 && response.data && response.data.content) {
-        const messageText = response.data.content.trim();
-        await api.sendMessage(messageText, threadID, messageID);
-        console.log('Sent answer as a reply to the user');
-      } else {
-        throw new Error('Invalid or missing response from API');
-      }
-    }
-  } catch (error) {
-    console.error(`Failed to get an answer: ${error.message}`);
-    api.sendMessage(
-      `${error.message}.\nAn error occurred fetching GPT API, please try again later.`,
-      threadID
-    );
+module.exports.run = async ({ api, event, args }) => {
+  if (args.length < 1) {
+    return api.sendMessage('Please provide a question\n\nExample:\nAi what is solar system?', event.threadID, event.messageID);
   }
-};;
+api.sendMessage("🔍 | AI is Searching and Typing your answer. Please Wait.....", event.threadID, event.messageID);
+  const content = encodeURIComponent(args.join(' '));
+  const apiUrl = `https://hazeyy-gpt4-api.kyrinwu.repl.co/api/gpt4/v-3beta?content=${content}`;
+
+  try {
+    const response = await axios.get(apiUrl);
+    const reply = response.data.reply;
+
+    api.sendMessage(reply, event.threadID, event.messageID);
+  } catch (error) {
+    console.error('Error fetching AI response:', error);
+    api.sendMessage('An error occurred while processing your request.', event.threadID, event.messageID);
+  }
+};
